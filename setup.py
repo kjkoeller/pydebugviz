e0758e392e0f0ac2864c37f7f2709e830b9eaf54
Kyle Koeller <kjkoeller@bsu.edu>
2025-04-17 16:35:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "pandas>=1.4.2",
         "ipywidgets>=8.1",
-        "networkx>=3.2",
+        "networkx>=3.0",
         "matplotlib>=3.5.1"
     ],
     python_requires=">=3.7",
