2abd7f11e5a3e0b486411dc35e05bc6d301635e3
Kyle Koeller <kjkoeller@bsu.edu>
2025-04-17 17:02:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydebugviz",
-    version="1.0.0",
+    version="1.0.1",
     description="Time-travel debugger and visualization toolkit for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
